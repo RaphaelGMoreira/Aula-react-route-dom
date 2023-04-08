de9dfb6f370cab1bebb53fad81323a91adade5db
RaphaelGMoreira <raphaelgimenesmoreira@gmail.com>
2023-04-08 12:12:28 -0300

    aula react route dom

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export const Post = () => {
+    return(
+        <div>
+            <h1>Post</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem saepe omnis ab numquam magni doloremque, vel sed illo vitae porro inventore odio quis perferendis reiciendis! Iste, similique nihil! Est, maxime.</p>
+        </div>    
+    );
+};
